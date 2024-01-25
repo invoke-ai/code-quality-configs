d426d544675d30cfa5fe3b1d98c29014a605771b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-25 21:15:17 +1100

    build: bail if tag already exists

--- scripts/tag_release_prettier-config-react.sh
+++ scripts/tag_release_prettier-config-react.sh
@@ -3,5 +3,11 @@
 # Parse version from package.json
 version=$(grep -oP '"version": "\K(.*?)(?=")' packages/prettier-config-react/package.json)
 tag="prettier-config-react@v$version"
+
+if git rev-parse "$tag" >/dev/null 2>&1; then
+    echo "Tag ${tag} already exists. Exiting..."
+    exit 1
+fi
+
 git tag $tag
 git push origin $tag
