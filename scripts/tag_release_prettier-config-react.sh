2bf61d25af3b49d2c6e46ebc2926b580d3de0767
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-25 21:17:45 +1100

    chore: format scripts

--- scripts/tag_release_prettier-config-react.sh
+++ scripts/tag_release_prettier-config-react.sh
@@ -5,8 +5,8 @@ version=$(grep -oP '"version": "\K(.*?)(?=")' packages/prettier-config-react/pac
 tag="prettier-config-react@v$version"
 
 if git rev-parse "$tag" >/dev/null 2>&1; then
-    echo "Tag ${tag} already exists. Exiting..."
-    exit 1
+  echo "Tag ${tag} already exists. Exiting..."
+  exit 1
 fi
 
 git tag $tag
